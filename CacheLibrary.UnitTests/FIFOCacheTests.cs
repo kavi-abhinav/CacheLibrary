74d2665a5cbf38b2f4ecea7edcf887503eaa14c8
kavi-abhinav <abhinavkavi@gmail.com>
2024-10-02 23:45:35 +0530

    add LRUCache tests and refactor tests

--- CacheLibrary.UnitTests/FIFOCacheTests.cs
+++ CacheLibrary.UnitTests/FIFOCacheTests.cs
@@ -4,83 +4,6 @@ namespace CacheLibrary.UnitTests
     {
         private CacheClient? _cacheClient;
 
-
-        [Theory]
-        [InlineData("name","Abhinav")]
-        [InlineData("steps", "500")]
-        [InlineData("occupation", "Software Engineer")]
-        public void CacheReturnsCorrectValue(string key, string value)
-        {
-            //setup
-            _cacheClient = new CacheClient(CachingStrategyOptions.FIFO);
-            _cacheClient.Add(key, value);
-
-            //execute
-            string fetchedValue = _cacheClient.Get(key);
-
-            //verify
-            Assert.Equal(value, fetchedValue);
-
-        }
-
-        [Fact]
-        public void CacheThrowsExceptionIfKeyAlreadyExists()
-        {
-            //setup
-            _cacheClient = new CacheClient(CachingStrategyOptions.FIFO);
-            _cacheClient.Add("test-key","value1");
-
-            //execute and verify
-            Assert.Throws<ArgumentException>(() => _cacheClient.Add("test-key", "value2"));
-
-        }
-
-        [Fact]
-        public void CacheThrowsExceptionIfKeyNotFound()
-        { 
-            //setup
-            _cacheClient = new CacheClient(CachingStrategyOptions.FIFO);
-
-            //execute and verify
-            Assert.Throws<ArgumentException>(() => _cacheClient.Get("non-existing-key"));
-
-        }
-
-
-        [Fact]
-        public void CacheThrowsExceptionDuringUpdateIfKeyNotFound()
-        {
-            //setup
-            _cacheClient = new CacheClient(CachingStrategyOptions.FIFO);
-
-            //execute and verify
-            Assert.Throws<ArgumentException>(() => _cacheClient.Update("non-existing-key", "hello"));
-
-        }
-
-        [Theory]
-        [InlineData("id","1","2")]
-        [InlineData("name", "abhinav", "Abhinav")]
-        public void CacheCorrectlyUpdatesValue(string key, string value, string updatedValue)
-        {
-            //setup
-            _cacheClient = new CacheClient(CachingStrategyOptions.FIFO);
-            _cacheClient.Add(key, value);
-
-            //execute
-            string fetchedValue = _cacheClient.Get(key);
-
-            //verify
-            Assert.Equal(value, fetchedValue);
-
-            //excecute
-            _cacheClient.Update(key,updatedValue);
-            fetchedValue = _cacheClient.Get(key);
-
-            //verify
-            Assert.Equal(updatedValue, fetchedValue);
-        }
-
         [Fact]
         public void CacheExpiresKeysBasedOnFIFOLogic()
         {
@@ -92,6 +15,47 @@ namespace CacheLibrary.UnitTests
 
             string fetchedValue = _cacheClient.Get("1");
             Assert.Equal("v1", fetchedValue);
+
+            _cacheClient.Add("4", "v4");
+
+            Assert.Throws<ArgumentException>(() => _cacheClient.Get("1"));
+        }
+
+
+        [Fact]
+        public void CacheExpiresFirstInKeyIrrespectiveOfGetOperation()
+        {
+            //setup
+            _cacheClient = new CacheClient(CachingStrategyOptions.FIFO, 3);
+            _cacheClient.Add("1", "v1");
+            _cacheClient.Add("2", "v2");
+            _cacheClient.Add("3", "v3");
+
+            string fetchedValue = _cacheClient.Get("1");
+
+            Assert.Equal("v1", fetchedValue); //get was successful
+
+            _cacheClient.Add("4", "v4");
+
+            Assert.Throws<ArgumentException>(() => _cacheClient.Get("1"));
+        }
+
+
+        [Fact]
+        public void CacheExpiresFirstInKeyIrrespectiveOfUpdateOperation()
+        {
+            //setup
+            _cacheClient = new CacheClient(CachingStrategyOptions.FIFO, 3);
+            _cacheClient.Add("1", "v1");
+            _cacheClient.Add("2", "v2");
+            _cacheClient.Add("3", "v3");
+
+            _cacheClient.Update("1", "v1.1");
+            _cacheClient.Update("1", "v1.2");
+            _cacheClient.Update("1", "v1.3");
+
+            string fetchedValue = _cacheClient.Get("1");
+            Assert.Equal("v1.3", fetchedValue); //Update was successful
 
             _cacheClient.Add("4", "v4");
 
---
+++ CacheLibrary.UnitTests/LRUCacheTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheLibrary.UnitTests
+{
+    public class LRUCacheTests
+    {
+        private CacheClient? _cacheClient;
+
+
+        [Fact]
+        public void CacheExpiresKeysBasedOnLRULogic()
+        {
+            //setup
+            _cacheClient = new CacheClient(CachingStrategyOptions.LRU, 3);
+            _cacheClient.Add("1", "v1");
+            _cacheClient.Add("2", "v2");
+            _cacheClient.Add("3", "v3");
+
+            _cacheClient.Add("4", "v4");
+
+            Assert.Throws<ArgumentException>(() => _cacheClient.Get("1")); //1 should have been evicted as per LRU
+        }
+
+        [Fact]
+        public void CacheExpiresKeysBasedOnLRULogicOnGet()
+        {
+            //setup
+            _cacheClient = new CacheClient(CachingStrategyOptions.LRU, 3);
+            _cacheClient.Add("1", "v1");
+            _cacheClient.Add("2", "v2");
+            _cacheClient.Add("3", "v3");
+            _cacheClient.Get("1");
+
+
+            _cacheClient.Add("4", "v4");
+
+            Assert.Throws<ArgumentException>(() => _cacheClient.Get("2")); //2 should have been evicted as per LRU
+        }
+
+        [Fact]
+        public void CacheExpiresKeysBasedOnLRULogicOnUpdates()
+        {
+            //setup
+            _cacheClient = new CacheClient(CachingStrategyOptions.LRU, 3);
+            _cacheClient.Add("1", "v1");
+            _cacheClient.Add("2", "v2");
+            _cacheClient.Add("3", "v3");
+            _cacheClient.Update("1", "v1.1");
+
+
+            _cacheClient.Add("4", "v4");
+
+            Assert.Throws<ArgumentException>(() => _cacheClient.Get("2")); //2 should have been evicted as per LRU
+        }
+
+    }
+}
